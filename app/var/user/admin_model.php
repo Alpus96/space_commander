dcba1e59a04f4269d289f156e192b14c94240a08
Alpus96 <alex.perme@gmail.com>
2017-11-11 17:06:26 +0100

    admin model skeleton

--- app/var/user/admin_model.php
+++ app/var/user/admin_model.php
@@ -6,5 +6,29 @@
             parent::__construct();
         }
 
+        protected function getUsers () {
+
+        }
+
+        protected function createNewUser ($user) {
+            
+        }
+
+        protected function toggleLock ($username) {
+            
+        }
+
+        protected function setType ($username, $new_type) {
+
+        }
+
+        protected function setPassword ($username, $new_pass) {
+
+        }
+
+        protected function delete ($username) {
+
+        }
+
     }
 ?>

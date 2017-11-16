69bd2d3bdb824127b3ec36d90f07882ef28a3e09
Alpus96 <alex.perme@gmail.com>
2017-11-16 12:14:32 +0100

    progress basic logger class

--- lib/srv/log.php
+++ lib/srv/log.php
@@ -5,11 +5,22 @@
         static private $abs_path;
 
         public function __construct ($logs_dir, $log_name) {
-            //  Concat absolute path and create file.
+            //  Concat absolute path and create file if necessary.
+			date_default_timezone_set('Europe/Stockholm');
+			$date = date('Y-m-d');
+			self::$path = __DIR__.'/logs/'.$date.'_'.$file_name.'.txt';
+			if (!file_exists(dirname(self::$path))) {
+                try { mkdir(dirname(self::$path)); }
+                catch (Exception $e)
+                { self::logException($e); }
+            }
         }
 
         public function writeLine ($line_str) {
-            if (!is_string($line_str)) { throw new InvalidArgumentException('writeLine($line_str): Parameter must be of type string.'); }
+            if (!is_string($line_str)) {
+                $msg = 'writeLine($line_str): Parameter must be of type string.';
+                throw new InvalidArgumentException($msg); 
+            }
         }
 
     }

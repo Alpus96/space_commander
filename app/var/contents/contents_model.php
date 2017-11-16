a2954b90c84e03676cc0763bb1882e733efa3097
Alpus96 <alex.perme@gmail.com>
2017-11-16 12:13:58 +0100

    content controller skeleton

--- app/var/contents/contents_model.php
+++ app/var/contents/contents_model.php
@@ -2,8 +2,33 @@
 
     class ContentsModel extends Mysql {
 
+        static private $query;
+
         protected function __construct () {
             parent::__construct();
+            self::$query = (object)[
+                'insert' => ''
+            ];
+        }
+
+        protected function insert ($marker, $text, $author) {
+
+        }
+
+        protected function selectMarker ($marker, $offset, $amount) {
+
+        }
+
+        protected function selectId ($id) {
+
+        }
+
+        protected function update ($id, $text) {
+
+        }
+
+        protected function moveToArchive ($id) {
+
         }
         
     }

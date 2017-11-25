0231018272df84ca7061bc2258da70b696f83c34
Alpus96 <alex.perme@gmail.com>
2017-11-25 10:47:47 +0100

    added comments

--- app/var/contents/contents_model.php
+++ app/var/contents/contents_model.php
@@ -1,5 +1,9 @@
 <?php
 
+    /**
+     * @todo indexing of contents. (?)
+     */
+    
     class ContentsModel extends Mysql {
 
         static private $query;
@@ -7,86 +11,99 @@
         protected function __construct () {
             parent::__construct();
             self::$query = (object)[
-                'insert' => ''
+                'insert' => '',
+                'selectMarker' => '',
+                'selectId' => '',
+                '' => ''
             ];
         }
 
+        /**
+         * Inserts a new entry to the database.
+         * 
+         * @param string $marker
+         * @param string $text
+         * @param string $author
+         * @return boolean
+         */
         protected function insert ($marker, $text, $author) {
-            //
+            //  Verify the passed parameters.
             if (!is_string($marker)) { return false; }
             if (!is_string($text)) { return false; }
             /**
-             * @todo Add relation between user and post. (+ define action to perform if user is deleted.)
+             * @todo Add relation between user and post. 
+             *       (+ define action to perform if user is deleted.)
+             *       To display correct authorname.
              */
             if (!is_string($author)) { return false; }
-            //
+            //  Connect to the database.
             $conn = parent::connect();
             if (!$conn) { return false; }
-            //
+            //  Run the query.
             if ($query = $conn->prepare(self::$query->insert)) {
                 $query->bind_param();
                 $query->execute();
-                //
+                //  Confirm the query had effect.
                 $was_successful = $query->affected_rows > 0 ? true : false;
-                //
+                //  Close the query and database connection before returning query success status.
                 $query->close();
                 $conn->close();
                 return $was_successful;
             }
-            //
+            //  If the query could not be run close the connection before returning success as false.
             $conn->close();
             return false;
         }
 
         protected function selectMarker ($marker, $offset, $amount) {
-            //
+            //  Verify the passed parameters.
             if (!is_string($marker)) { return false; }
             if (!is_integer($offset)) { return false; }
             if (!is_integer($amount)) { return false; }
-            //
+            //  Connect to the databse.
             $conn = parent::connect();
             if (!$conn) { return false; }
-            //
+            //  Run the query.
             if ($query = $conn->prepare(self::$query->selectMarker)) {
                 $query->bind_param();
                 $query->execute();
-                //
+                //  Get the result data.
                 $query->bind_result();
                 $data = [];
                 while ($query->fetch()) {
                     $entry = (object)[];
                     array_push($result, $entry);
                 }
-                //
+                //  Close the query and the database connection before returning the data.
                 $query->close();
                 $conn->close();
                 return count($result) > 0 ? $result : null;
             }
-            //
+            //  If the query could not be run close the connection and return success false.
             $conn->close();
             return false; 
         }
 
         protected function selectId ($id) {
-            //
+            //  Verify the passed parameter.
             if (!is_integer($id)) { return false; }
-            //
+            //  Connect to the database.
             $conn = parent::connect();
             if (!$conn) { return false; }
-            //
+            //  Run the query.
             if ($query = $conn->prepare(self::$query->selectId)) {
                 $query->bind_param();
                 $query->execute();
-                //
+                //  Get the result data.
                 $query->bind_result();
                 $query->fetch();
                 $result = (object)[];
-                //
+                //  Close the query and database connection before returning the data.
                 $query->close();
                 $conn->close();
                 return $result->text != '' ? $result : null;
             }
-            //
+            //  If the query could not be run close the database connection and return success false.
             $conn->close();
             return false;
         }
